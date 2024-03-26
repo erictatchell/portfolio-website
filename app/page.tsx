92daa7113a7cb582a4b649d7d675b0ffec7875be
Eric Tatchell <erictatch@gmail.com>
2024-03-25 22:42:45 -0700

    for real this time fixed spacing issues (?)

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,15 @@ import Content from '@/components/content';
 
 export default function Home() {
   return (
-    <div className="flex flex-col md:grid md:grid-cols-5 lg:grid-cols-8  w-full h-screen items-center justify-center">
-      <div className="flex-1 md:col-span-5">
+    <div className="flex flex-col md:grid md:grid-cols-5 lg:grid-cols-8 w-full min-h-screen md:h-auto items-center justify-center">
+      <div className="flex-1 md:col-span-5 lg:col-span-7">
         <Content />
       </div>
 
-      <span className="hidden md:block md:w-px bg-gray-500 self-center h-96 mb-4 lg:mb-0"></span>
+      {/* This line can be made conditional or adjusted so it doesn't enforce a large height on mobile */}
+      <span className="hidden md:block md:w-px bg-gray-500 self-center h-auto md:h-96 mb-4 lg:mb-0"></span>
 
-      <div className="flex w-full justify-center space-x-4 px-4 mb-36 md:space-x-0 md:px-0 md:justify-between md:col-span-2 lg:mb-0">
+      <div className="flex w-full justify-center space-x-4 px-4 mb-4 md:space-x-0 md:px-0 md:justify-between md:col-span-2 lg:mb-0 lg:col-span-1">
         <Menu />
       </div>
     </div>

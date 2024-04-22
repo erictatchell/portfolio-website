cd620dcf8d098f2cea0bcfb32cb40ddea3c03dbb
Eric Tatchell <erictatch@gmail.com>
2024-04-21 22:22:09 -0700

    padding update for mobile chrome v2

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Content from '@/components/content';
 export default function Home() {
   /* https://stackoverflow.com/questions/66556514/tailwind-grid-template-columns */
   return (
-    <div className="grid lg:grid-cols-[75%_25%] md:grid-cols-[60%_40%] h-screen">
+    <div className="grid bg-contain lg:grid-cols-[75%_25%] md:grid-cols-[60%_40%] h-screen">
       <div className='flex md:items-center lg:px-16 justify-center'>
         <Content />
       </div>

33dbc05b4beafddecf61f15ec7f2c247f7cd422b
erictatchell <erictatch@gmail.com>
2024-09-20 22:09:07 -0700

    removed mui reference

--- components/menu/socials.tsx
+++ components/menu/socials.tsx
@@ -1,7 +1,6 @@
 import { GithubLogo, LinkedinLogo } from "@phosphor-icons/react";
 import Email from "./email";
 import { useState } from "react";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Socials() {
 
@@ -15,7 +14,7 @@ export default function Socials() {
                 className="hover:bg-[#1F2937] border-[#e4e4e7] border-2 p-1 inline-flex justify-center items-center text-base font-medium text-center"
                 onMouseEnter={() => setIsGithubHovered(true)}
                 onMouseLeave={() => setIsGithubHovered(false)}>
-                <GithubLogo size={24} weight="fill" color='#e4e4e7'/>
+                <GithubLogo size={24} weight="fill" color='#e4e4e7' />
             </a>
             <a href="https://www.linkedin.com/in/etatchell/"
                 className="hover:bg-blue-500 border-[#e4e4e7] border-2 inline-flex justify-center items-center text-base font-medium text-center"
